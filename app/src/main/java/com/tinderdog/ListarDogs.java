7f0b84f97d3a0255dcf531137fc0b5b2283cbf6a
Marina Pontes <marinaptenorio@hotmail.com>
2019-09-28 12:46:06 -0300

    Telas DetailDog,ListarDog e Nova MainActivity

---
+++ app/src/main/java/com/tinderdog/ListarDogs.java
@@ -0,0 +1,14 @@
+package com.tinderdog;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class ListarDogs extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_listar_dogs);
+    }
+}

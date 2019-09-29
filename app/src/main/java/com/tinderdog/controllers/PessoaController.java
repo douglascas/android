1c299d1ab47690a793d000681167da6a6cf4fd2d
joaoeffetive <joaopedro.triun@gmail.com>
2019-09-29 14:07:09 -0300

    Implementacao do Controller Pessoa

--- app/src/main/java/com/tinderdog/controllers/PessoaController.java
+++ app/src/main/java/com/tinderdog/controllers/PessoaController.java
@@ -1,4 +1,72 @@
 package com.tinderdog.controllers;
 
-public class PessoaController {
+import com.tinderdog.controllers.api.IPessoaController;
+import com.tinderdog.repository.api.IPessoaRepository;
+import com.tinderdog.models.usuario.Pessoa;
+import com.tinderdog.repository.exception.pessoa.InsertPessoaException;
+import com.tinderdog.repository.exception.pessoa.PessoaNotFoundException;
+import com.tinderdog.repository.exception.pessoa.UpdatePessoaException;
+
+public class PessoaController implements IPessoaController {
+    private static PessoaController instance;
+    private IPessoaRepository repositorio;
+
+    public static PessoaController getInstance() {
+        if (instance == null) {
+            instance = new PessoaController();
+        }
+        return instance;
+    }
+
+    @Override
+    public void get(Pessoa pessoa) throws PessoaNotFoundException {
+        repositorio.get(pessoa);
+    }
+
+    @Override
+    public void getAll() {
+        repositorio.getAll();
+    }
+
+    @Override
+    public void getAll(int init, int end) {
+        repositorio.getAll(init,end);
+    }
+
+    @Override
+    public void getById(int id) throws PessoaNotFoundException {
+        if(id <= 0){
+            throw new PessoaNotFoundException();
+        }
+        repositorio.getById(id);
+    }
+
+    @Override
+    public void update(Pessoa pessoa) throws UpdatePessoaException{
+        if(pessoa == null){
+            throw new UpdatePessoaException();
+        }
+        repositorio.update(pessoa);
+    }
+
+    @Override
+    public void insert(Pessoa pessoa) throws InsertPessoaException {
+        if(pessoa == null){
+            throw new InsertPessoaException();
+        }
+        repositorio.insert(pessoa);
+    }
+
+    @Override
+    public void delete(Pessoa pessoa) throws PessoaNotFoundException {
+        if(pessoa == null){
+            throw new PessoaNotFoundException();
+        }
+        repositorio.delete(pessoa);
+    }
+
+    @Override
+    public void deleteById(int pessoa) throws PessoaNotFoundException {
+        repositorio.deleteById(pessoa);
+    }
 }
--- app/src/main/java/com/tinderdog/controllers/api/IPessoaController.java
+++ app/src/main/java/com/tinderdog/controllers/api/IPessoaController.java
@@ -1,4 +1,19 @@
 package com.tinderdog.controllers.api;
 
+import com.tinderdog.models.usuario.Pessoa;
+import com.tinderdog.repository.exception.pessoa.InsertPessoaException;
+import com.tinderdog.repository.exception.pessoa.PessoaNotFoundException;
+import com.tinderdog.repository.exception.pessoa.UpdatePessoaException;
+
+import java.util.List;
+
 public interface IPessoaController {
+    void get(Pessoa pessoa) throws PessoaNotFoundException;
+    void getAll();
+    void getAll(int init, int end);
+    void getById(int id) throws PessoaNotFoundException;
+    void update(Pessoa pessoa) throws UpdatePessoaException;
+    void insert(Pessoa pessoa) throws InsertPessoaException;
+    void delete(Pessoa pessoa) throws PessoaNotFoundException;
+    void deleteById(int pessoa) throws PessoaNotFoundException;
 }

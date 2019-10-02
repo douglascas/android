8eed13a0168a8991ca8e64a6ac4e8dcb226dee0c
Arthur L <arthurifccl@gmail.com>
2019-10-02 18:17:33 -0300

    Renames

--- app/src/main/java/com/tinderdog/ui/ActionChoiceActivity.java
+++ app/src/main/java/com/tinderdog/ui/ActionChoiceActivity.java
@@ -2,21 +2,16 @@ package com.tinderdog.ui;
 
 import android.os.Bundle;
 
-import com.google.android.material.floatingactionbutton.FloatingActionButton;
-import com.google.android.material.snackbar.Snackbar;
 import com.tinderdog.R;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.widget.Toolbar;
-
-import android.view.View;
 
 public class ActionChoiceActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_cadastro_pessoa);
+        setContentView(R.layout.activity_choice);
     }
 
 }

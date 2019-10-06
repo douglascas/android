64b81e4c5afbb020d5e18b63c8ee8cbf6e827969
Marina Pontes <marinaptenorio@hotmail.com>
2019-10-05 23:04:39 -0300

    Novas telas : RegisterDog ,Main nova

---
+++ app/src/main/java/com/tinderdog/ui/dogs/RegisterDogActivity.java
@@ -0,0 +1,14 @@
+package com.tinderdog.ui.dogs;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class RegisterDogActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_register_dog);
+    }
+}

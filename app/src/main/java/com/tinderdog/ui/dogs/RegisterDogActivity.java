ab1eb1f52a33cb60f4900f6e1eb2ccb7170c47dc
joaoeffetive <joaopedro.triun@gmail.com>
2019-10-07 22:13:15 -0300

    Implementacao do botao de upload de imagem para o dog

--- app/src/main/java/com/tinderdog/ui/dogs/RegisterDogActivity.java
+++ app/src/main/java/com/tinderdog/ui/dogs/RegisterDogActivity.java
@@ -2,11 +2,16 @@ package com.tinderdog.ui.dogs;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Bitmap;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 
 import com.tinderdog.R;
 import com.tinderdog.controllers.Facade;
@@ -20,13 +25,19 @@ import com.tinderdog.repository.factoy.PessoaRepositoryFactory;
 import com.tinderdog.ui.login.LoginActivity;
 import com.tinderdog.ui.pessoa.RegisterPessoaActivity;
 
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
 public class RegisterDogActivity extends AppCompatActivity {
 
     private Button mbtnCadastro;
+    private Button mbtnUpload;
+    private ImageView imageUpload;
     private EditText etNome;
     private EditText etCorPelagem;
     private EditText etIdade;
     private EditText etPorte;
+    public static final int GET_FROM_GALLERY = 3;
     private Facade facade = Facade.getInstance();
 
 
@@ -49,8 +60,12 @@ public class RegisterDogActivity extends AppCompatActivity {
         etCorPelagem = findViewById(R.id.txtPwd);
         etIdade = findViewById(R.id.txtName);
         etPorte = findViewById(R.id.txtData);
+        mbtnUpload = findViewById(R.id.btnUpload);
+        imageUpload = findViewById(R.id.image);
 
-
+        mbtnUpload.setOnClickListener(v -> {
+            startActivityForResult(new Intent(Intent.ACTION_PICK, android.provider.MediaStore.Images.Media.INTERNAL_CONTENT_URI), GET_FROM_GALLERY);
+        });
 
         mbtnCadastro.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -72,5 +87,23 @@ public class RegisterDogActivity extends AppCompatActivity {
                 }
             }
         });
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+
+        if(requestCode==GET_FROM_GALLERY && resultCode == Activity.RESULT_OK) {
+            Uri selectedImage = data.getData();
+            Bitmap bitmap = null;
+            try {
+                bitmap = MediaStore.Images.Media.getBitmap(this.getContentResolver(),selectedImage);
+                imageUpload.setImageBitmap(bitmap);
+            }catch (FileNotFoundException e){
+                e.printStackTrace();
+            } catch (IOException e) {
+                e.printStackTrace();
+            }
+        }
     }
 }
